c6b0974ffea287f2e09ce3d735abe1ee0de38b74
Summerev <ayoon1227a@naver.com>
2025-06-10 11:03:34 +0900

    Delete apps directory

--- apps/chatbot/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def chat_interface(request):
-    return render(request, 'chatbot/chat.html')
--- apps/main/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.chatbot_view, name='chatbot'),  # 루트로 접속 시 chatbot.html 띄움
-]
