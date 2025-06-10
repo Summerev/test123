9bbbc3f9f652a5a396dded5dc8749260a15ad8c9
Summerev <ayoon1227a@naver.com>
2025-06-10 17:30:45 +0900

    Delete teamproject-main directory

--- teamproject-main/legal_web/apps/rag/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def rag_query(request):
-    return render(request, 'rag/rag_query.html')
--- teamproject-main/legal_web/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.chatbot.urls')),  # ✅ chatbot.urls로 연결돼 있어야 함
-    path('', include('chatbot.urls')),
-]
